20327e403194530458d761ec33ee04093bfb3836
Louis Munier <louis.munier@epfl.ch>
2019-06-04 23:58:58 +0200

    Working implementation on cuda of the brute-force algorithm.

--- src/constants.hpp
+++ src/constants.hpp
@@ -26,24 +26,24 @@
 /**
  * Different defines to have constants for the overall project.
  */
-//#define PRINT                          /**< if it is defined, print value in terminal and csv file */
+#define PRINT                          /**< if it is defined, print value in terminal and csv file */
 
 #define G 6.67408e-11f                  /**< gravitational general constant */
 #define EPSILON 0.000005f               /**< to avoid collision between particles */
 
 #define DIM_3 3                        /**< number of dimension in 3D */
 
-#define NB_PARTICLES 10                /**< number of particles for the project */
-#define SIDE 100000.0f                      /**< side of the area considered for the project */
+#define NB_PARTICLES 1000                /**< number of particles for the project */
+#define SIDE 10.0f                      /**< side of the area considered for the project */
 #define SIDE_2f SIDE / 2.0f                      /**< side of the area considered for the project */
 #define SIDE_4f SIDE / 4.0f                      /**< side of the area considered for the project */
 #define SHIFT SIDE/3.0f                 /**< shift each particles to unbalanced probability of particles position */
 #define OCCUPATION_PERC 0.5f            /**< percentage of occupation to avoid particle go easily outside boundaries */
-#define MASS_MAX 10e10f                 /**< maximum of mass value for a particle */
+#define MASS_MAX 10e5f                 /**< maximum of mass value for a particle */
 
 #define MAX_THREADS 1024
 
-#define DELTA_T 0.1f                   /**< duration of each update timestep */
-#define ITERATIONS 100                /**< number of iterations to solve nBody problem */
+#define DELTA_T 0.05f                   /**< duration of each update timestep */
+#define ITERATIONS 1000                /**< number of iterations to solve nBody problem */
 
 #endif //PROJECT_CONSTANTS_HPP
