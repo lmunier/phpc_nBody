cb9fc616d73fd5ebed277e4829daa0a7b184aad5
lmunier <louis.munier@epfl.ch>
2019-06-06 20:09:47 +0200

    Updates after first tests on cluster.

--- start_nbody.sh
+++ start_nbody.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --nodes=1
-#SBATCH --time=1:0:0
+#SBATCH --time=3:0:0
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --qos=gpu_free
@@ -9,10 +9,10 @@
 source /ssoft/spack/bin/slmodules.sh -s  x86_E5v2_Mellanox_GPU
 
 module load gcc cuda
-
 module list
 
 make cleanall
 make
-srun  ./nbody
 
+srun  ./nbody < data/10000_part.in > data/10000_results.txt
+srun  ./nbody < data/100000_part.in > data/100000_results.txt
